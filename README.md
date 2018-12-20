9f363b683a8d55292bb75cb184cd179ec9a0f1ef
Cody Sehl <cody.sehl@gmail.com>
2018-12-20 07:51:49 -0700

    wip

---
+++ README.md
@@ -0,0 +1,9 @@
+# Kotlin Coroutines in Android
+
+A playground to understand Kotlin Coroutines
+
+## Running
+
+- Browse `git log` and see which commit you want to take a look at
+- Run the app
+- Set logcat filter to `Error` and `Show only selected application`
--- app/src/main/java/net/codysehl/coroutinessharedprefs/MainActivity.kt
+++ app/src/main/java/net/codysehl/coroutinessharedprefs/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -17,15 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         val sharedPreferencesWrapper = SharedPreferencesWrapper(this)
 
-        println("Activity thread: ${Thread.currentThread()}")
+        Log.e("CoroutineSharedPrefs", "Activity thread: ${Thread.currentThread()}")
 
         activity_main_button.setOnClickListener {
-            println("onClickListener thread: ${Thread.currentThread()}")
+            Log.e("CoroutineSharedPrefs", "onClickListener thread: ${Thread.currentThread()}")
             GlobalScope.launch(Dispatchers.Main) {
-                println("coroutine start thread: ${Thread.currentThread()}")
+                Log.e("CoroutineSharedPrefs", "coroutine start thread: ${Thread.currentThread()}")
                 val value = sharedPreferencesWrapper.writeValue("hello world")
                 activity_main_text.text = value
-                println("coroutine end thread: ${Thread.currentThread()}")
+                Log.e("CoroutineSharedPrefs", "coroutine end thread: ${Thread.currentThread()}")
             }
         }
     }
@@ -36,13 +37,13 @@ class SharedPreferencesWrapper(context: Context) {
 
     @SuppressLint("ApplySharedPref")
     fun writeValue(value: String): String {
-        println("shared prefs start thread: ${Thread.currentThread()}")
+        Log.e("CoroutineSharedPrefs", "shared prefs start thread: ${Thread.currentThread()}")
         sharedPreferences.edit().apply {
             (0..50000).forEach {
                 putString("key$it", value + it + Math.random())
             }
         }.commit()
-        println("shared prefs end thread: ${Thread.currentThread()}")
+        Log.e("CoroutineSharedPrefs", "shared prefs end thread: ${Thread.currentThread()}")
         return Math.random().toString()
     }
 }
